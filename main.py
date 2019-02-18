0f085a1c50ec9d824eca7d64983bee7497e05153
lukehalley <luke123halley@gmail.com>
2019-02-18 19:11:10 +0000

    getting gps and printing it out every 5 seconds

--- main.py
+++ main.py
@@ -1,65 +1,50 @@
-from network import WLAN
+from network import Sigfox
 from pytrack import Pytrack
 import urequests as requests
 from L76GNSS import L76GNSS
 import socket
 import time
 import pycom
+import struct
 
 py = Pytrack()
-gps = L76GNSS(py, timeout=30)
+gps = L76GNSS(py, timeout=60)
 
-# Your WiFi network credentials
-WIFI_SSID = 'lhk-wifi' 
-WIFI_KEY = 'lukepycom'
+init_timer = time.time()
 
-# Get this from the Wia dashboard
-DEVICE_SECRET_KEY = 'd_sk_fXao9ZXMX6MDdFHAPhkdd55Z'
+# print("connecting to Sigfox")
+# init Sigfox for RCZ1 (Europe)
+# sigfox = Sigfox(mode=Sigfox.SIGFOX, rcz=Sigfox.RCZ1)
 
-# Delay between each event
-DELAY = 30
+# create a Sigfox socket
+# s = socket.socket(socket.AF_SIGFOX, socket.SOCK_RAW)
+# make the socket blocking
+# s.setblocking(True)
 
-wlan = WLAN(mode=WLAN.STA)
-nets = wlan.scan()
+# s.setsockopt(socket.SOL_SIGFOX, socket.SO_RX, False)
 
-# Connect to the WiFi network
-for net in nets:
-    if net.ssid == WIFI_SSID:
-        print('Network found!')
-        wlan.connect(net.ssid, auth=(net.sec, WIFI_KEY), timeout=5000)
-        print('Connecting...')
-        while not wlan.isconnected():
-             machine.idle() # save power while waiting
-        print('WLAN connection succeeded!')
-        break
-
-
-# Post an Event to the Wia cloud
+# Post an location to the Wia cloud via Sigfox backend
 def post_location(latitude, longitude):
     try:
-        url = "https://api.wia.io/v1/locations"
-        headers = {"Authorization": "Bearer " + DEVICE_SECRET_KEY, "Content-Type": "application/json"}
-        json_data = {"latitude": str(latitude), "longitude": str(longitude)}
-        if json_data is not None:
-            req = requests.post(url=url, headers=headers, json=json_data)
-            if req.status_code is not 200:
-                machine.reset()
-            else:
-                print(json_data)
-            return req.json()
-        else:
-            pass
+        print(str(latitude), ":", str(longitude))
+        # s.send(struct.pack('f',float(latitude)) + struct.pack('f',float(longitude)))
     except:
+        print("Failed to get lat long")
         pass
-
-lat = 53
-lng = -6
 
 # main loop
 while True:
+    final_timer = time.time()
+    diff = final_timer - init_timer
     # Get coordinates from pytrack
-    coord = gps.coordinates(debug=True)
-    if not coord == (None, None):
-        lat, lng = coord
+    coord = gps.coordinates()
+
+    lat, lng = coord
     post_location(lat, lng)
-    time.sleep(2)
+    init_timer = time.time()
+    time.sleep(5) 
+    # If the GPS has coordinates and 15 minites has past. Post the location data
+    # if not coord == (None, None) and diff < 15:
+    #     lat, lng = coord
+    #     post_location(lat, lng)
+    #     init_timer = time.time()
