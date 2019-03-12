7f04c7ae9ab1ba7fc557cca4cc6c0988f3df0407
lukehalley <luke123halley@gmail.com>
2019-03-12 21:52:24 +0800

    set three limits, ok, warning and danger. Counting them and printing the count of them at the end

--- acc.py
+++ acc.py
@@ -1,31 +1,59 @@
- import time
+# Imports
+import time
 from LIS2HH12 import LIS2HH12
 from pytrack import Pytrack
 import time
 import gc
 
+# Init of the Pytrack sensors
 py = Pytrack()
 acc = LIS2HH12()
 
+# Array to collect two pitch and roll values to compare
 pitchValues = []
 rollValues = []
 
+# Init the timer
 init_timer = time.time()
+
+# Setting the mishandle limits
+okLimit = 30
+warningLimit = 55
+dangerLimit = 100
+
+# Mishandle Counts
+warnCount = 0
+dangerCount = 0
 
 while True:
     final_timer = time.time()
     diff = final_timer - init_timer
-    pitch = acc.pitch()
-    roll = acc.roll()
     if diff > 60:
         init_timer = time.time()
         print("One Minute Passed!")
+        print(
+            "Mishandle Counts - Warning Count: {} Danger Count: {} ".format(
+                warnCount, dangerCount
+            )
+        )
         time.sleep(5)
     else:
         while len(pitchValues) <= 1 and len(rollValues) <= 1:
             pitchValues.append(acc.pitch())
             rollValues.append(acc.roll())
-        print("2 Values In -> {},{}".format(pitchValues, rollValues))
+        else:
+            diff = abs(pitchValues[1] - pitchValues[0]) + abs(
+                rollValues[1] - rollValues[0]
+            )
+            print("Diff: {}".format(diff))
+            if diff > okLimit and diff < dangerLimit:
+                print("Mishandle Warning: 002")
+                warnCount += 1
+                time.sleep(1)
+            elif diff > warningLimit:
+                dangerCount += 1
+                print("Mishandle Danger: 003")
+            else:
+                pass
         pitchValues.clear()
         rollValues.clear()
-        time.sleep(1)
