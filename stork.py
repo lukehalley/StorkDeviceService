c9033ea56a1c9a17bd0adc344bb05f9cfc50f847
lukehalley <luke123halley@gmail.com>
2019-04-20 18:14:25 +0100

    fixed interval

--- stork.py
+++ stork.py
@@ -62,7 +62,7 @@ mishandle = False
 minInt = 0
 
 # Send every n mins - WARNING: Should be 10 minutes to meet the Sigfox sending limits
-sendCycle = 0
+sendCycle = 10
 
 # GPS Fix Status
 fix = False
